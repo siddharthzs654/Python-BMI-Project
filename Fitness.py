2c677ce7adc1d39733428dc2505d4366f86c57ad
Shubhra1028 <shubhra1028@gmail.com>
2018-11-16 00:27:02 +0530

    DONE

--- Fitness.py
+++ Fitness.py
@@ -17,6 +17,11 @@ g = ""
 r = 0
 name = ""
 age = ""
+rbc = ""
+wbc = ""
+hb = ""
+platelets = ""
+report = ""
     
 class HomeScreen(Screen):
     def validateName(self, nameInput):
@@ -88,6 +93,93 @@ class WeightScreen(Screen):
     
     pass
 
+class OptionalScreen(Screen):
+    def getWbc(self, wbcInput):
+        global wbc, age
+        try:
+            wbc = int(wbcInput)
+        except:
+            popup = Popup(title='Error',
+                    content=Label(text='WBC can only contain numbers' ),
+                    size_hint=(None, None), size=(400, 100))
+            popup.open()
+
+        if(age == 0):
+            if(wbc<9000):
+                wbc="Low"
+            elif(wbc<30000):
+                wbc="Normal"
+            else:
+                wbc="High"
+        elif(age < 2):
+            if(wbc<6200):
+                wbc="Low"
+            elif(wbc<17000):
+                wbc="Normal"
+            else:
+                wbc="High"
+        if(age >=2):
+            if(wbc<5000):
+                wbc="Low"
+            elif(wbc<10000):
+                wbc="Normal"
+            else:
+                wbc="High"
+
+    def getPlatelets(self, plInput):
+        global platelets
+        try:
+            platelets = int(plInput)
+        except:
+            popup = Popup(title='Error',
+                    content=Label(text='WBC can only contain numbers' ),
+                    size_hint=(None, None), size=(400, 100))
+            popup.open()
+
+        if(platelets<150000):
+            platelets = "Low"
+        elif(platelets<450000):
+            platelets = "Normal"
+        else:
+            platelets = "High"
+            
+    def result(self, rbcIN, hbIN):
+        global rbc, wbc, hb, platelets, g
+        rbc = float(rbcIN)
+        hb = float(hbIN)
+        # RBC CALCULATIONS
+        if(g == "male"):
+            if(rbc<4.7):
+                rbc="Low"
+            elif(rbc<6.1):
+                rbc="Normal"
+            else:
+                rbc="High"
+        elif(g == "female"):
+            if(rbc<4.2):
+                rbc="Low"
+            elif(rbc<5.4):
+                rbc="Normal"
+            else:
+                rbc="High"
+
+        # HB CALCULATIONS
+        if(g == "male"):
+            if(hb<13.5):
+                hb="Low"
+            elif(hb<17.5):
+                hb="Normal"
+            else:
+                hb="High"
+        elif(g == "female"):
+            if(hb<12.0):
+                hb="Low"
+            elif(hb<15.5):
+                hb="Normal"
+            else:
+                hb="High"
+    pass
+
 class ResultScreen(Screen):
     #gf.graphs(h,w)    # Create a image of graph with name graphpic.png, Now display as you want
     bmi = float(0)
@@ -104,19 +196,29 @@ class ResultScreen(Screen):
             ResultScreen.category = "Over Weight"
         else:
             ResultScreen.category = "Obese"
-        label.text = str(ResultScreen.category) + " with " + str(r)
         print(label)
         gf.graphs(h,w)
 
-    pass
+        # Making the report
 
-class OptionalScreen(Screen):
+        global report
+        report = "Dear "+name+"\nYou are "+str(ResultScreen.category) + " with " + str(r)+"."
+        if(rbc != ""):
+            report+= "\nYour RBC count is " + rbc
+        if(wbc != ""):
+            report+= "\nYour WBC count is " + wbc
+        if(hb != ""):
+            report+= "\nYour Hemoglobin is " + hb
+        if(platelets != ""):
+            report+= "\nYour platelets count is " + platelets
+        label.text = report
+
     pass
 
 class ScreenManagement(ScreenManager):
     pass
  
-class KivyTut2App(App):
+class KivyFITNESSApp(App):
     def build(self):
         Window.clearcolor = (1, 1, 1, 1)
         return
